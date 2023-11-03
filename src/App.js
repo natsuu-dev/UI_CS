e186258bb064516616335a9b86e5e8435179fca5
natsu <108961905+natsuu-dev@users.noreply.github.com>
2023-11-03 03:22:09 -0400

    Update

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from './assets/header';
+import MainContent from './assets/mainsection';
+import Footer from './assets/footer';
+
+import './index.css';
+function App() {
+  return (
+<div>
+  <Header />
+  <MainContent />
+  <Footer />
+</div>
+  );
+}
+
+export default App;
